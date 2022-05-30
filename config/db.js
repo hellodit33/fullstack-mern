984e25804e0cfc99e54eacb53cb4ac4b8ef991ac
hellodit33 <45536957+hellodit33@users.noreply.github.com>
2022-05-30 10:34:58 +0200

    all pushed

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-.connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.oyapy.mongodb.net/onboarding",
-)
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log("Failed to connect to MongoDB", err));
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb+srv://" +
+        process.env.DB_USER_PASS +
+        "@cluster0.oyapy.mongodb.net/onboarding"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
