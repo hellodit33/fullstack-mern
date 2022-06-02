aaf9fb6bb837ca926f0770bb9d6515bce6d82f5e
hellodit33 <45536957+hellodit33@users.noreply.github.com>
2022-06-02 20:57:11 +0200

    new push

--- utils/multer.js
+++ utils/multer.js
@@ -1,16 +1,14 @@
 const multer = require("multer");
 const path = require("path");
-
-//multer config
+// Multer config
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("this file type is not supported"), false);
+      cb(new Error("File type is not supported"), false);
       return;
     }
-    //file format supported, callback accepts the file so the boolean is true
     cb(null, true);
   },
 });
